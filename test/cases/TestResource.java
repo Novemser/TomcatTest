69566fb02969c9ea3b8f4f62992f1ba688772028
Novemser <2072806652@qq.com>
2017-05-30 00:38:15 +0800

    Test case template modified

---
+++ test/cases/TestResource.java
@@ -0,0 +1,46 @@
+package cases;
+
+import org.apache.jasper.el.ELContextImpl;
+import org.apache.log4j.Logger;
+
+import javax.el.ELContext;
+import javax.el.ExpressionFactory;
+import javax.el.ValueExpression;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Project: apache-tomcat-7.0.0-src
+ * Package: cases
+ * Author:  Novemser
+ * 2017/5/29
+ */
+public class TestResource {
+    public static final Logger logger = Logger.getLogger(TestResource.class);
+
+    /**
+     * 表达式测试
+     *
+     * @param expression 表达式
+     * @param expected   预期的值
+     */
+    public static void testExpression(String passTestCase, String expression, String expected) {
+        ExpressionFactory factory = ExpressionFactory.newInstance();
+        ELContext context = new ELContextImpl();
+        try {
+            ValueExpression ve = factory.createValueExpression(
+                    context, expression, String.class);
+
+            String result = (String) ve.getValue(context);
+
+
+            assertEquals(expected, result);
+        } catch (Throwable e) {
+            logger.error("Exception caught in EL expression test:", e);
+            return;
+        }
+
+
+        logger.info(passTestCase + " passed.");
+    }
+}
---
+++ test/cases/novemser/el/value/arithmetic/TestDN210001.java
@@ -0,0 +1,70 @@
+package cases.novemser.el.value.arithmetic;
+
+import cases.TestResource;
+import org.junit.Test;
+
+/**
+ * Project: apache-tomcat-7.0.0-src
+ * Package: cases.novemser.el.literal.arithmetic
+ * Author:  Novemser
+ * 2017/5/29
+ */
+public class TestDN210001 {
+    @Test
+    public void test0001() {
+        TestResource.testExpression("DN-21-0001",
+                "${(2*2)%3%2}", "1");
+    }
+
+    @Test
+    public void test0002() throws Exception {
+        TestResource.testExpression("DN-21-0002",
+                "${2*2+2*2}", "8"
+        );
+    }
+
+    @Test
+    public void test0003() throws Exception {
+        TestResource.testExpression("DN-21-0003",
+                "${2+2+20%2}", "4"
+        );
+    }
+
+    @Test
+    public void test0004() throws Exception {
+        TestResource.testExpression("DN-21-0004",
+                "${2m2*20%2}", ""
+        );
+    }
+
+    @Test
+    public void test0005() throws Exception {
+        TestResource.testExpression("DN-21-0005",
+                "${2+2m20%2}", ""
+        );
+    }
+
+    @Test
+    public void test0006() throws Exception {
+        TestResource.testExpression("DN-21-0006",
+                "${2+2*20j2}", ""
+        );
+    }
+
+    @Test
+    public void test0007() throws Exception {
+        TestResource.testExpression("DN-21-0007",
+                "${(1+0+2+2*2)%3}", "1");
+    }
+
+    @Test
+    public void test0008() throws Exception {
+        TestResource.testExpression("DN-21-0008",
+                "${(1*0*2+2*2)%3}", "1");
+    }
+    @Test
+    public void test0009() throws Exception {
+        TestResource.testExpression("DN-21-0009",
+                "${(1%10+2*20)%6%3}", "2");
+    }
+}
